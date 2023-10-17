af8956a52c18cf5108b518ac6526dc6ab4f07463
lucasBdev <luccacpm@gmail.com>
2023-10-17 15:11:13 -0300

    feat: add-swagger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json'
 import alunoRoutes from './routes/AlunoRouter';
-
 import * as dotenv from 'dotenv';
+
 
 
 dotenv.config(); 
 
 const app = express();
+
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(express.json());
 
@@ -16,5 +21,5 @@ app.use(cors());
 app.use(alunoRoutes);
 
 app.listen(3000, () => {
-  console.log('Servidor executando na porta 3000');
+  console.log('Servidor executado na porta 3000');
 });
